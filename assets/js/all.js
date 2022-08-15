75bb5fb3cdc62d404ebe3a6e56fa87511681baa9
gwen <gwan1017@gmail.com>
2022-08-15 22:30:45 +0800

    Update 2022-08-15T14:30:36.823Z

---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+ClassicEditor.create(document.querySelector('#editor')).then(function (editor) {
+  console.log(editor);
+})["catch"](function (error) {
+  console.error(error);
+});
+//# sourceMappingURL=all.js.map
